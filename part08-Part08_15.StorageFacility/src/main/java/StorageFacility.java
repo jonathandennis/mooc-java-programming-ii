c2d50ab845eae122789caeffc58977d443804229
jonathandennis <4jondennis@gmail.com>
2021-05-07 09:44:56 +0200

    Finish part8

--- part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
+++ part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -2,32 +2,36 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class StorageFacility {
-    private HashMap<String, ArrayList<String>> storageUnit;
+    private HashMap<String, ArrayList<String>> storageUnits;
     
     public StorageFacility() {
-        this.storageUnit = new HashMap<>();
+        this.storageUnits = new HashMap<>();
     }
     
     public void add(String unit, String item) {
-        this.storageUnit.putIfAbsent(unit, new ArrayList<>());       
-        this.storageUnit.get(unit).add(item);
+        this.storageUnits.putIfAbsent(unit, new ArrayList<>());       
+        this.storageUnits.get(unit).add(item);
     }
     
     public ArrayList<String> contents(String unit) {
-        return this.storageUnit.getOrDefault(unit, new ArrayList<>());
+        return this.storageUnits.getOrDefault(unit, new ArrayList<>());
     }
     
     public void remove(String unit, String item) {
-        this.storageUnit.get(unit).remove(item);
+        if (!this.storageUnits.containsKey(unit)) {
+            return;
+        }
+        
+        this.storageUnits.get(unit).remove(item);
+        
+        if(this.storageUnits.get(unit).isEmpty()) {
+            this.storageUnits.remove(unit);
+        }
     }
     
     public ArrayList<String> storageUnits() {
-        ArrayList<String> allUnits = new ArrayList<>();
-        
-        for (String unit : storageUnit.keySet()) {
-            if (storageUnit.get(unit).isEmpty()) {
-                continue;
-            }
+        ArrayList<String> allUnits = new ArrayList<>();        
+        for (String unit : this.storageUnits.keySet()) {           
             allUnits.add(unit);
         }
         return allUnits;
